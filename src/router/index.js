e608e0bcc4b4d42e2f78fa5bd9bf7803fd2645a9
wwc <2543816873@qq.com>
2018-06-17 09:47:15 +0800

    first create create Login.vue Regedit.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
 import Regedit from '@/components/Regedit'
 import GetPassword from '@/components/GetPassword'
@@ -8,11 +7,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/he',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/Login',
       name: 'Login',
@@ -27,6 +21,10 @@ export default new Router({
       path: '/GetPassword',
       name: 'GetPassword',
       component: GetPassword
+    },
+    {
+      path:'/',
+      redirect:'/Login'
     }
   ]
 })
